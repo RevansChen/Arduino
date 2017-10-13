ce6ce65ad7e0e2ce372841ffc64411943f7a7a17
RevansChen <d79523@hotmail.com>
2017-10-13 20:34:35 +0800

    程式碼排版及重構
    縮排調整為4個空白
    重複的程式碼以迴圈與副程式方式重構

--- flappy-bird/flappy-bird.ino
+++ flappy-bird/flappy-bird.ino
@@ -1,450 +1,298 @@
 #include <SPI.h>
-
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+
+//                lcd_Addr,En,Rw,Rs,d4,d5,d6,d7,backlighPin,pol
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); 
 
-int y_val,y,y_data;
+
+// 接腳
+const byte PIN_POWER = 12;  // 電源開關
+const byte PIN_RESET = 9;   // 重置
+
+const byte PIN_CDS = A0;    // 
+const byte PIN_PAD_Y = A1;  // 
+
+const byte PIN_LEVEL_SELECT = 2;    // 難度選擇
+const byte PIN_LEVEL_EASY = 4;      //     簡單
+const byte PIN_LEVEL_MIDDLE = 7;    //     普通
+const byte PIN_LEVEL_HARD = 8;      //     困難
+
+const byte PIN_RGBLED_RED = 3;      // 三色LED, 紅色
+const byte PIN_RGBLED_GREEN = 5;    // 三色LED, 綠色
+const byte PIN_RGBLED_BLUE = 6;     // 三色LED, 藍色
+
+const byte MAX7219_REGADDR_DIGIT_BASEADDR = 0x01;   // 資料暫存器開始位址
+const byte MAX7219_REGADDR_DECODE_MODE = 0x09;      // 解碼模式暫存器
+const byte MAX7219_REGADDR_INTENSITY = 0x0a;        // 亮度暫存器
+const byte MAX7219_REGADDR_SCAN_LIMIT = 0x0b;       // 掃描行數暫存器
+const byte MAX7219_REGADDR_SHUTDOWN = 0x0c;         // 關閉模式暫存器
+const byte MAX7219_REGADDR_DISPLAY_TEST = 0x0f;     // 測試模式暫存器
+
+const byte MAX7219_SCANLIMIT_8 = 0x07;          // 掃描0~7位
+const byte MAX7219_INTESITY_8 = 0x08;           // 中等亮度, duty cycle = 17/32
+const byte MAX7219_DECODE_MODE_UNUSED = 0x00;   // 不使用BCD解碼
+const byte MAX7219_DISPLAY_TEST_OFF = 0x00;     // 停止 (正常工作下使用這個值)
+const byte MAX7219_SHUTDOWN_OFF = 0x01;         // 停止 (正常工作下使用這個值)
+
+const int LEVEL_EASY = 1;
+const int LEVEL_MIDDLE = 2;
+const int LEVEL_HARD = 3;
+
+const byte xsy[8] = {
+    MAX7219_REGADDR_DIGIT_BASEADDR, 
+    MAX7219_REGADDR_DIGIT_BASEADDR + 1,
+    MAX7219_REGADDR_DIGIT_BASEADDR + 2, 
+    MAX7219_REGADDR_DIGIT_BASEADDR + 3, 
+    MAX7219_REGADDR_DIGIT_BASEADDR + 4, 
+    MAX7219_REGADDR_DIGIT_BASEADDR + 5, 
+    MAX7219_REGADDR_DIGIT_BASEADDR + 6, 
+    MAX7219_REGADDR_DIGIT_BASEADDR + 7
+};
+
+const byte ysy[8] = {
+    B00000001,
+    B00000010,
+    B00000100,
+    B00001000,
+    B00010000,
+    B00100000,
+    B01000000,
+    B10000000
+};
+
+const byte ylevel[6] = {
+    B11111000, 
+    B11110001, 
+    B11100011, 
+    B11000111, 
+    B10001111, 
+    B00011111
+};
+
+bool gamest = false;
+bool start = false;
+
+int y_data = 0;
 int score = 0;
-boolean start,easy,middle,hard,select,rst,gamest = 0;
 
-unsigned int ran,cds;
-const byte xsy[8] = {1,2,3,4,5,6,7,8};
-const byte ysy[8] = {1,2,4,8,16,32,64,128};
-const byte ylevel[6] = {248,241,227,199,143,31};
-//byte buf[8]={0,0,0,0,0,0,0,0};
+byte g_bright = 0;
+byte r_bright = 0;
+byte b_bright = 0;
 
-byte g_bright;
-byte r_bright;
-byte b_bright;
-
-const byte noop = 0x0;
-const byte decod = 0x9;
-const byte inten = 0xa;
-const byte scanl = 0xb;
-const byte shutd = 0xc;
-const byte displ = 0xf;
-
-void max7219 (const byte reg,const byte data){
-  digitalWrite(SS,LOW);
-  SPI.transfer(reg);
-  SPI.transfer(data);
-  digitalWrite(SS,HIGH);
+void max7219(const byte reg, const byte data) {
+    digitalWrite(SS, LOW);
+    SPI.transfer(reg);
+    SPI.transfer(data);
+    digitalWrite(SS, HIGH);
 }
 
 void setup() {
-Serial.begin(9600);
-pinMode(12,INPUT);	//power-光遮斷
-pinMode(4,INPUT);   //easy- 
-pinMode(7,INPUT);	//middle- 
-pinMode(8,INPUT);	//hard - 
-pinMode(2,INPUT);	//select -button
-pinMode(9,INPUT);	//reset
-pinMode(5,OUTPUT);	//Green		level-----easy
-pinMode(3,OUTPUT);	//Red		level-----hard
-pinMode(6,OUTPUT);	//Blue		level-----middle
-pinMode(A0,INPUT);	//cds select
+    Serial.begin(9600);
+    
+    pinMode(PIN_POWER, INPUT);  // 電源開關
+    pinMode(PIN_RESET, INPUT);  // 重置
+    
+    pinMode(PIN_CDS, INPUT);    // cds select
+    pinMode(PIN_PAD_Y, INPUT);  // 搖桿Y軸
+    
+    pinMode(PIN_LEVEL_SELECT, INPUT);   // 難度選擇
+    pinMode(PIN_LEVEL_EASY, INPUT);     // 簡單
+    pinMode(PIN_LEVEL_MIDDLE, INPUT);   // 普通
+    pinMode(PIN_LEVEL_HARD, INPUT);     // 困難
+    
+    pinMode(PIN_RGBLED_GREEN, OUTPUT);  // 三色LED, 綠色  level- easy
+    pinMode(PIN_RGBLED_BLUE, OUTPUT);   // 三色LED, 藍色  level- middle
+    pinMode(PIN_RGBLED_RED, OUTPUT);    // 三色LED, 紅色  level- hard
+    
+    // 初始化 LCD, 一行 16 的字元, 共 2 行, 預設開啟背光
+    lcd.begin(16, 2);      
+    lcd.backlight();
+    lcd.clear();
 
-lcd.begin(16, 2);      // 初始化 LCD，一行 16 的字元，共 2 行，預設開啟背光
-lcd.backlight();
-lcd.clear();
+    // 初始化MAX7219
+    SPI.begin();
+    max7219(MAX7219_REGADDR_SCAN_LIMIT, MAX7219_SCANLIMIT_8);
+    max7219(MAX7219_REGADDR_DECODE_MODE, MAX7219_DECODE_MODE_UNUSED);
+    max7219(MAX7219_REGADDR_INTENSITY, MAX7219_INTESITY_8);
+    max7219(MAX7219_REGADDR_DISPLAY_TEST, MAX7219_DISPLAY_TEST_OFF);
+    max7219(MAX7219_REGADDR_SHUTDOWN, MAX7219_SHUTDOWN_OFF);
 
-SPI.begin();
-
-max7219(scanl,7);
-max7219(decod,0);
-max7219(inten,8);
-max7219(displ,0);
-max7219(shutd,1);
-
-for(byte i =0 ; i < 8; i++){
-    max7219(i+1,0);
-	}
-  
+    for (byte i = 0; i < 8; i++) {
+        max7219(MAX7219_REGADDR_DIGIT_BASEADDR + i, 0);
+    }
 }
 
 void loop() {
-  start = !digitalRead(12);
+    start = !digitalRead(PIN_POWER);
   
-  if(start == 0){
-    //Serial.println("Please remove the card to start the game");
-	
-	//lcd.clear();
-	lcd.setCursor(0, 0);
-	lcd.print("remove card to");
-	lcd.setCursor(0, 1);
-	lcd.print("start the game");
- }
-  if(start == 1){
-		lcd.setCursor(0, 0);
-		lcd.print("~~~~~Flappy~~~~~");
-		lcd.setCursor(0, 1);
-		lcd.print("~~~~~~bird~~~~~~");
-    selectlevel();
-  }
-}
-//123456
-void selectlevel(){
-	
-    //Serial.println("~flappy bird~");
-    
-	color();
-	
-	gamest = 0;
-	score = 0;
-	Serial.println(cds);
-	//Serial.println(cds);
-      /*Serial.print("easy");
-      Serial.println(easy);
-      Serial.print("midd");
-      Serial.println(middle);
-      Serial.print("hard");
-      Serial.println(hard);
-      Serial.println(start);*/
-      while(cds > 85 && cds < 100){     //level-----easy
-        start = !digitalRead(12);
-        select = !digitalRead(2);
-        Serial.println("you select---easy");
-        Serial.println("yes or no");
-		lcd.setCursor(0, 0);
-		lcd.print("******easy******");
-		lcd.setCursor(0, 1);
-        lcd.print("***yes or no ***");
-		analogWrite(5,g_bright); //93
-		analogWrite(3,r_bright); // 110
-		analogWrite(6,b_bright); // 75
-		r_bright = 0;
-		g_bright = 0;
-		b_bright = 0;
-        while(select == 1){                           // start game			
-			Serial.println("game start");
-		if(gamest == 0){
-			lcd.setCursor(0, 0);
-			lcd.print("***game start***");
-			lcd.setCursor(0, 1);
-			lcd.print("                ");
-			}
-			rst = !digitalRead(9);
-			game3();
-			//break;
-          if(rst == 0 || start == 0)break;
-        }
-        //Serial.println(start);
-		rst = !digitalRead(9);
-        if(rst == 0 || start == 0)break;
-      }
-      while(cds > 100 && cds < 120){     //level-----middle
-        start = !digitalRead(12);
-        select = !digitalRead(2);
-        Serial.println("you select---middle");
-        Serial.println("yes or no");
-		lcd.setCursor(0, 0);
-		lcd.print("*****middle*****");
-		lcd.setCursor(0, 1);
-        lcd.print("***yes or no ***");
-		analogWrite(5,g_bright); 
-		analogWrite(3,r_bright); 
-		analogWrite(6,b_bright); 
-		r_bright = 0;
-		g_bright = 0;
-		b_bright = 0;
-        while(select == 1){                           //start game			
-			Serial.println("game start");
-		if(gamest == 0){
-			lcd.setCursor(0, 0);
-			lcd.print("***game start***");
-			lcd.setCursor(0, 1);
-			lcd.print("                ");
-			}
-			rst = !digitalRead(9);
-			game2();
-			
-          if(rst == 0 || start == 0)break;
-        }
-        //Serial.println(start);
-        rst = !digitalRead(9);
-        if(rst == 0 || start == 0)break;
-      }
-      while( cds > 65 && cds < 85 ){     //level-----hard
-        start = !digitalRead(12);
-        select = !digitalRead(2);
-        Serial.println("you select---hard");
-        Serial.println("yes or no");
-		lcd.setCursor(0, 0);
-		lcd.print("******hard******");
-		lcd.setCursor(0, 1);
-        lcd.print("***yes or no ***");
-		analogWrite(5,g_bright); //88
-		analogWrite(3,r_bright); //77
-		analogWrite(6,b_bright); //102
-		r_bright = 0;
-		g_bright = 0;
-		b_bright = 0;
-        while(select == 1){                           //start game
-			Serial.println("game start");
-			if(gamest == 0){
-			lcd.setCursor(0, 0);
-			lcd.print("***game start***");
-			lcd.setCursor(0, 1);
-			lcd.print("                ");
-			}
-			rst = !digitalRead(9);
-			game1();
-			//break;
-          if(rst == 0 || start == 0)break;
-        }
-        //Serial.println(start);
-        rst = !digitalRead(9);
-        if(rst == 0 || start == 0)break;
-      }
-
-}
-
-void color(){
-	 cds = analogRead(A0);
-	easy = !digitalRead(4);  //G
-    middle = !digitalRead(7);  //B
-    hard = digitalRead(8);  //R
-	analogWrite(5,g_bright); //88
-	analogWrite(3,r_bright); //77
-	analogWrite(6,b_bright); //102
-	if (easy == 0){
-		r_bright = 0;
-		g_bright = 255;
-		b_bright = 0;
-	}
-	
-	if (middle == 0){
-		r_bright = 0;
-		g_bright = 0;
-		b_bright = 255;
-	}
-	
-	if (hard == 0){
-		r_bright = 255;
-		g_bright = 0;
-		b_bright = 0;
-	}
-}
-
-void ctrly(){
-    y_val = analogRead(A1);
-
-    if(y_val<300) y = -1;
-    else if(y_val>700) y =  1;
-    else y = 0;
-
-    if(y == 1){
-      y_data ++;
-      delay(10);
+    if (start) {
+        lcd.setCursor(0, 0);
+        lcd.print("~~~~~Flappy~~~~~");
+        lcd.setCursor(0, 1);
+        lcd.print("~~~~~~bird~~~~~~");
+        selectlevel();
     }
-    if(y == -1){
-      y_data --;
-      delay(10);
-    }    
+    else {
+        lcd.setCursor(0, 0);
+        lcd.print("remove card to");
+        lcd.setCursor(0, 1);
+        lcd.print("start the game");
+    }
+}
 
-    if(y_data>7) y_data=7;
-    if(y_data<0) y_data=0;
-    max7219(xsy[0],ysy[y_data]);
+void selectlevel() {
+    color();
+    
+    gamest = 0;
+    score = 0;
+    
+    unsigned int cds = analogRead(PIN_CDS);
+    Serial.println(cds);
+    
+    gameloop(85, 100, cds, LEVEL_EASY);     // level-----easy
+    gameloop(100, 120, cds, LEVEL_MIDDLE);  // level-----middle
+    gameloop(65, 85, cds, LEVEL_HARD);      // level-----hard
+}
+
+void gameloop(int cdsMin, int cdsMax, int cds, int v) {
+    String msg1;
+    String msg2;
+    int delayTime;
+    switch (v) {
+        case LEVEL_EASY:
+            msg1 = "easy";
+            msg2 = "******easy******";
+            delayTime = 150;
+            break;
+            
+        case LEVEL_MIDDLE:
+            msg1 = "middle";
+            msg2 = "*****middle*****";
+            delayTime = 100;
+            break;
+            
+        case LEVEL_HARD:
+            msg1 = "hard";
+            msg2 = "******hard******";
+            delayTime = 60;
+            break;
+    }
+    
+    bool rst;
+    while (cdsMin < cds && cds < cdsMax) { 
+        Serial.print("you select---");
+        Serial.println(msg1);
+        Serial.println("yes or no");
+        
+        lcd.setCursor(0, 0);
+        lcd.print(msg2);
+        lcd.setCursor(0, 1);
+        lcd.print("***yes or no ***");
+        
+        analogWrite(PIN_RGBLED_GREEN, g_bright);
+        analogWrite(PIN_RGBLED_RED, r_bright);
+        analogWrite(PIN_RGBLED_BLUE, b_bright);
+        r_bright = 0;
+        g_bright = 0;
+        b_bright = 0;
+        
+        start = !digitalRead(PIN_POWER);
+        bool select = !digitalRead(PIN_LEVEL_SELECT);
+        
+        // game start
+        while (select) {                           
+            Serial.println("game start");
+            if (!gamest) {
+                lcd.setCursor(0, 0);
+                lcd.print("***game start***");
+                lcd.setCursor(0, 1);
+                lcd.print("                ");
+            }
+            rst = !digitalRead(PIN_RESET);
+            game(delayTime);
+            
+            if (!(rst && start)) break;
+        }
+        
+        rst = !digitalRead(PIN_RESET);
+        if (!(rst && start)) break;
+    }
+}
+
+void color() {
+    analogWrite(PIN_RGBLED_GREEN, g_bright);
+    analogWrite(PIN_RGBLED_RED, r_bright);
+    analogWrite(PIN_RGBLED_BLUE, b_bright);
+    
+    bool easy = !digitalRead(PIN_LEVEL_EASY);       // G
+    if (!easy) {
+        r_bright = 0;
+        g_bright = 255;
+        b_bright = 0;
+    }
+    
+    bool middle = !digitalRead(PIN_LEVEL_MIDDLE);   // B
+    if (!middle) {
+        r_bright = 0;
+        g_bright = 0;
+        b_bright = 255;
+    }
+    
+    bool hard = digitalRead(PIN_LEVEL_HARD);        // R
+    if (!hard) {
+        r_bright = 255;
+        g_bright = 0;
+        b_bright = 0;
+    }
+}
+
+void ctrly() {
+    int y_val = analogRead(PIN_PAD_Y);
+    
+    if (y_val < 300) {
+        if (y_data > 0) {
+            y_data--;
+        }
+    }
+    else if(y_val > 700) {
+        if (y_data < 7) {
+            y_data++;
+        }
+    }
+
+    max7219(xsy[0], ysy[y_data]);
     delay(10);
 }
 
-void game1(){
-      randomSeed(analogRead(A5)); 
-      ran = random(0,6);
-      if(gamest == 0){
-        for(int x = 7 ; x > -0.1 ; x--){
-          ctrly();
-          max7219(xsy[x],ylevel[ran]);
-          if(x < 7)max7219(xsy[x+1],0);
-          delay(60);
-          if(x == 0 && (y_data==0 || y_data==1 || y_data==2) && ran == 0){
-            score++;
-          }else if(x == 0 && (y_data!=0 || y_data!=1 || y_data!=2) && ran == 0){
-            gamest = 1;
-            break;
-          }
-      
-          if(x == 0 && (y_data==1 || y_data==2 || y_data==3) && ran == 1){
-            score++;
-          }else if(x == 0 && (y_data!=1 || y_data!=2 || y_data!=3) && ran == 1){
-            gamest = 1;
-            break;
-          }
-      
-          if(x == 0 && (y_data==2 || y_data==3 || y_data==4) && ran == 2){
-            score++;
-          }else if(x == 0 && (y_data!=2 || y_data!=3 || y_data!=4) && ran == 2){
-            gamest = 1;
-            break;
-          }
-      
-          if(x == 0 && (y_data==3 || y_data==4 || y_data==5) && ran == 3){
-            score++;
-          }else if(x == 0 && (y_data!=3 || y_data!=4 || y_data!=5) && ran == 3){
-            gamest = 1;
-            break;
-          }
-      
-          if(x == 0 && (y_data==4 || y_data==5 || y_data==6) && ran == 4){
-            score++;
-          }else if(x == 0 && (y_data!=4 || y_data!=5 || y_data!=6) && ran == 4){
-            gamest = 1;
-            break;
-          }
-      
-          if(x == 0 && (y_data==5 || y_data==6 || y_data==7) && ran == 5){
-            score++;
-          }else if(x == 0 && (y_data!=5 || y_data!=6 || y_data!=7) && ran == 5){
-            gamest = 1;
-            break;
-          }
+void game(int delayTime) {
+    randomSeed(analogRead(A5)); 
+    int ran = (int)random(0, 6);
+    if (!gamest) {
+        for (int x = 7; x > 0; x--) {
+            ctrly();
+            max7219(xsy[x], ylevel[ran]);
+            if (x < 7) {
+                max7219(xsy[x + 1], 0);
+            }
+            delay(delayTime);
+            
+            if (x == 0 && (y_data == ran || y_data == (ran + 1) || y_data == (ran + 2))) {
+                score++;
+            }
+            else if (x == 0 && (y_data != ran || y_data != (ran + 1) || y_data != (ran + 2))) {
+                gamest = true;
+                break;
+            }
         }
-      }
-	  if(gamest == 1){
+    }
+    
+    if (gamest) {
         Serial.println(score);
-			lcd.setCursor(0, 0);
-		lcd.print("your bird died..");
-		lcd.setCursor(0, 1);
+        lcd.setCursor(0, 0);
+        lcd.print("your bird died..");
+        lcd.setCursor(0, 1);
         lcd.print("your score : ");
-		lcd.print(score);
-	  }
+        lcd.print(score);
+    }
 }
-
-void game2(){
-      randomSeed(analogRead(A5)); 
-      ran = random(0,6);
-      if(gamest == 0){
-        for(int x = 7 ; x > -0.1 ; x--){
-          ctrly();
-          max7219(xsy[x],ylevel[ran]);
-          if(x < 7)max7219(xsy[x+1],0);
-          delay(100);
-          if(x == 0 && (y_data==0 || y_data==1 || y_data==2) && ran == 0){
-            score++;
-          }else if(x == 0 && (y_data!=0 || y_data!=1 || y_data!=2) && ran == 0){
-            gamest = 1;
-            break;
-          }
-      
-          if(x == 0 && (y_data==1 || y_data==2 || y_data==3) && ran == 1){
-            score++;
-          }else if(x == 0 && (y_data!=1 || y_data!=2 || y_data!=3) && ran == 1){
-            gamest = 1;
-            break;
-          }
-      
-          if(x == 0 && (y_data==2 || y_data==3 || y_data==4) && ran == 2){
-            score++;
-          }else if(x == 0 && (y_data!=2 || y_data!=3 || y_data!=4) && ran == 2){
-            gamest = 1;
-            break;
-          }
-      
-          if(x == 0 && (y_data==3 || y_data==4 || y_data==5) && ran == 3){
-            score++;
-          }else if(x == 0 && (y_data!=3 || y_data!=4 || y_data!=5) && ran == 3){
-            gamest = 1;
-            break;
-          }
-      
-          if(x == 0 && (y_data==4 || y_data==5 || y_data==6) && ran == 4){
-            score++;
-          }else if(x == 0 && (y_data!=4 || y_data!=5 || y_data!=6) && ran == 4){
-            gamest = 1;
-            break;
-          }
-      
-          if(x == 0 && (y_data==5 || y_data==6 || y_data==7) && ran == 5){
-            score++;
-          }else if(x == 0 && (y_data!=5 || y_data!=6 || y_data!=7) && ran == 5){
-            gamest = 1;
-            break;
-          }
-          /*if (score == 100){
-            Serial.println("level3");
-            score = 0;
-            game1();
-          }*/
-        }
-      }
-        Serial.println(score);
-		if(gamest == 1){
-        Serial.println(score);
-			lcd.setCursor(0, 0);
-		lcd.print("your bird died..");
-		lcd.setCursor(0, 1);
-        lcd.print("your score : ");
-		lcd.print(score);
-	  }
-}
-
-void game3(){
-      randomSeed(analogRead(A5)); 
-      ran = random(0,6);
-      if(gamest == 0){
-        for(int x = 7 ; x > -0.1 ; x--){
-          ctrly();
-          max7219(xsy[x],ylevel[ran]);
-          if(x < 7)max7219(xsy[x+1],0);
-          delay(150);
-          if(x == 0 && (y_data==0 || y_data==1 || y_data==2) && ran == 0){
-            score++;
-          }else if(x == 0 && (y_data!=0 || y_data!=1 || y_data!=2) && ran == 0){
-            gamest = 1;
-            break;
-          }
-      
-          if(x == 0 && (y_data==1 || y_data==2 || y_data==3) && ran == 1){
-            score++;
-          }else if(x == 0 && (y_data!=1 || y_data!=2 || y_data!=3) && ran == 1){
-            gamest = 1;
-            break;
-          }
-      
-          if(x == 0 && (y_data==2 || y_data==3 || y_data==4) && ran == 2){
-            score++;
-          }else if(x == 0 && (y_data!=2 || y_data!=3 || y_data!=4) && ran == 2){
-            gamest = 1;
-            break;
-          }
-      
-          if(x == 0 && (y_data==3 || y_data==4 || y_data==5) && ran == 3){
-            score++;
-          }else if(x == 0 && (y_data!=3 || y_data!=4 || y_data!=5) && ran == 3){
-            gamest = 1;
-            break;
-          }
-      
-          if(x == 0 && (y_data==4 || y_data==5 || y_data==6) && ran == 4){
-            score++;
-          }else if(x == 0 && (y_data!=4 || y_data!=5 || y_data!=6) && ran == 4){
-            gamest = 1;
-            break;
-          }
-      
-          if(x == 0 && (y_data==5 || y_data==6 || y_data==7) && ran == 5){
-            score++;
-          }else if(x == 0 && (y_data!=5 || y_data!=6 || y_data!=7) && ran == 5){
-            gamest = 1;
-            break;
-          }
-          /*if (score == 100){
-            Serial.println("level2");
-            score = 0;
-            game2();
-          }*/
-        }
-      }
-        Serial.println(score);
-		if(gamest == 1){
-        Serial.println(score);
-			lcd.setCursor(0, 0);
-		lcd.print("your bird died..");
-		lcd.setCursor(0, 1);
-        lcd.print("your score : ");
-		lcd.print(score);
-	  }
-}
-
-
